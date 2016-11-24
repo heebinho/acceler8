ad9a863399d3f93e5d9b5903d977d47503843c74
heebinho <renatoheeb@gmail.com>
2016-11-24 19:30:06 +0100

    Team details

--- app/models/Team.java
+++ app/models/Team.java
@@ -1,7 +1,6 @@
 package models;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import javax.persistence.*;
@@ -17,11 +16,20 @@ import play.data.validation.Constraints.*;
 @Entity
 public class Team extends Model {
 	
+	public Team(){}
 	
 	@Required
 	private String name;
 	
-    @ManyToMany(cascade = CascadeType.ALL)
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(
             name = "TeamMember",
             joinColumns = @JoinColumn(name = "team_id"),
@@ -38,14 +46,6 @@ public class Team extends Model {
 		this.users = users;
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	private String description;
 	
 	
@@ -56,18 +56,50 @@ public class Team extends Model {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
 	
-	public Team(String name, String description){
-		this.name = name;
-		this.description = description;
+	private String city;
+	
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	private String country;
+	
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	private String website;
+	
+	public String getWebsite() {
+		return website;
+	}
+
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+	
+	private boolean isPrivate = false;
+	
+	public boolean isPrivate() {
+		return isPrivate;
+	}
+
+	public void setIsPrivate(boolean isPrivate) {
+		this.isPrivate = isPrivate;
 	}
 	
 	@Override
 	public String toString() {
 		return String.format("%s - %s", getId(), getName());
 	}
-	
-	public Team(){}
 
 }
