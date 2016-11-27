47de68ae2234ea85d2d70dbc2213f1e4302bd5f3
Marco Gantenbein <m.gantenbein@gmx.ch>
2016-11-27 14:21:51 +0100

    Refactor

--- app/controllers/MyTeamController.java
+++ app/controllers/MyTeamController.java
@@ -3,31 +3,17 @@
  */
 package controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.inject.Inject;
-
-import controllers.account.ResetController.AskForm;
-import javastrava.api.v3.auth.TokenManager;
-import javastrava.api.v3.auth.model.Token;
-import javastrava.api.v3.auth.ref.AuthorisationScope;
-import javastrava.api.v3.model.StravaAthlete;
-import javastrava.api.v3.service.Strava;
-import models.Invite;
 import models.Team;
 import models.User;
 import models.dao.ITeamDao;
 import models.dao.TeamDao;
-import play.data.Form;
-import play.data.FormFactory;
 import play.db.jpa.Transactional;
 import play.mvc.Result;
 import play.mvc.Security;
 import services.account.AccountService;
 import services.account.IAccountService;
-import services.team.ITeamService;
-import services.team.TeamService;
 import views.html.myteam.*;
 
 
--- app/models/User.java
+++ app/models/User.java
@@ -1,7 +1,7 @@
 package models;
 
 import java.util.HashSet;
-import java.util.List;
+
 import java.util.Set;
 
 import javax.persistence.*;
